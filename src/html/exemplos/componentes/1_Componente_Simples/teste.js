92ea91941e04db4fec0fa84eff49d3a390b51b21
Bonafé <bonafe@opapagaiorinoceronte.net>
2024-03-04 15:43:49 -0300

    Organização dos exemplos de componentes HTML

--- src/html/exemplos/componentes/1_Componente_Simples/teste.js
+++ src/html/exemplos/componentes/1_Componente_Simples/teste.js
@@ -5,6 +5,4 @@ export class OlaMundo extends HTMLElement{
         this.innerText = 'Olá mundo!';              
     }
 }
-console.log ("vai definir o custom element");
 customElements.define('abelinha-olamundo', OlaMundo);
-console.log ("Definiu o custom element");
